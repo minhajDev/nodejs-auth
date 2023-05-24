b3fca2b63ead2ae238afaebcbf99ceaf72c379fc
Minhaj Ur Rehman <115610484+minhajDev@users.noreply.github.com>
2023-05-24 05:37:02 -0700

    update

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,34 +1,18 @@
+const axios = require("axios");
+
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
 };
 
-const http = require('http');
 
 exports.getUsers = (req, res) => {
-  const options = {
-    hostname: 'jsonplaceholder.typicode.com',
-    path: '/users',
-    method: 'GET'
-  };
-
-  const request = http.request(options, (response) => {
-    let data = '';
-
-    response.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      const users = JSON.parse(data);
+  axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      const users = response.data;
       res.status(200).send({ users });
+    })
+    .catch(error => {
+      console.error("Error retrieving users:", error);
+      res.status(500).send({ message: "Error retrieving users" });
     });
-  });
-
-  request.on('error', (error) => {
-    console.error('Error retrieving users:', error);
-    res.status(500).send({ message: 'Error retrieving users' });
-  });
-
-  request.end();
 };
-
